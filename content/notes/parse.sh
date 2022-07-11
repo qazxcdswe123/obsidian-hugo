a6c37a1284227f3dfae29743733da117bd274240
Neil-Chen <v009008777@hotmail.com>
2022-07-11 17:10:30 +0800

    mv dir

--- content/notes/parse.sh
+++ content/notes/parse.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-for f in notes/*.md;
+for f in *.md;
 do
   topLine=$(sed -n 1p "$f")
-  new=$(echo "$f" | cut -d '.' -f 1 | cut -d '/' -f 2)
+  new=$(echo "$f" | cut -d '.' -f 1)
   if [ "$topLine" != "---" ]
   then
     sed -i "1s/^/---\ntitle: $new\n---\n/" "$f"
